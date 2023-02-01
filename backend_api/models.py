3d1bfe7f5393eb35965d735f2918d7689404d8a8
Jordan-Burgess <alfredjordan93@gmail.com>
2023-02-01 00:05:36 -0500

    Create profile model with basic user info

--- backend_api/models.py
+++ backend_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.CharField(max_length=200)
+    profession = models.CharField(max_length=200)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.first_name
