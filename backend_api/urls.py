b2b56a19eef97c0d60acd4ff21678589afbd8763
Jordan-Burgess <alfredjordan93@gmail.com>
2023-02-01 00:34:11 -0500

    Update pipfile with djangorestframework dependencies

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', views.Users.as_view(), name="users"),
 ]
