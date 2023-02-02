18f95b4b9bd0541a86872f463677f44555aab55c
jenna <parkerjenna98@gmail.com>
2023-02-02 17:03:43 -0500

    configures my machine to open routes

--- backend_api/models.py
+++ backend_api/models.py
@@ -9,3 +9,29 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.first_name
+        
+class Project(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    TOPIC_CHOICES = [
+        ('ART', 'Art'),
+        ('CAR', 'Career'),
+        ('COM', 'Community'),
+        ('CUL', 'Culture'),
+        ('ECO','Environmental'),
+        ('EDU', 'Education'),
+        ('FNC','Finance'),
+        ('FOD', 'Food'),
+        ('MED', 'Media/Pop Culture'),
+        ('MD', 'Medical'),
+        ('REL', 'Religion'),
+        ('SOC', 'Social'),
+        ('SJ','Social Justice'),
+        ('SPT', 'Sports'),
+        ('IT', 'Technology'),
+        ('TRV', 'Travel'),
+    ]
+    title = models.CharField(max_length=50)
+    topic = models.CharField(max_length=3,choices=TOPIC_CHOICES)
+    
+
+    
