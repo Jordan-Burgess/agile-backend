35248855b6a84baefd5538b4f494c40aaceb4836
jenna <parkerjenna98@gmail.com>
2023-02-02 17:22:52 -0500

    creates project route

--- backend_api/views.py
+++ backend_api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from rest_framework.views import APIView
 from django.http import JsonResponse, HttpResponse
-from .serializers import ProfileSerializer
-from .models import Profile
+from .serializers import ProfileSerializer, ProjectSerializer
+from .models import Profile, Project
 
 class Info(View):
     def get(self, request):
@@ -16,3 +16,10 @@ class Users(APIView):
         data = Profile.objects.all()
         serializer = ProfileSerializer(data, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+class Projects(APIView):
+    def get(self, request):
+        data = Project.objects.all()
+        serializer = ProjectSerializer(data, many=True)
+        return JsonResponse(serializer.data, safe=False)
+        
