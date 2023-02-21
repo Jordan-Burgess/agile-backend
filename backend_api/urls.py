1d84f83829e76eab78005a87db48c8c0dcf42ca1
Jordan-Burgess <alfredjordan93@gmail.com>
2023-02-21 12:33:13 -0500

    Change users url parameter from integer to string

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.Info.as_view(), name="info"),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', views.Users.as_view(), name="users"),
-    path('users/<int:id>', views.ProfileInfo.as_view(), name="users"),
+    path('users/<str:id>', views.ProfileInfo.as_view(), name="users"),
     path('projects/', views.Projects.as_view(), name="projects" ),
     path('projects/<int:pk>',views.ProjectDetails.as_view()),
 ]
